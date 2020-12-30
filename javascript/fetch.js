f19a463e82b4507f9ede63b9cd41289424620f66
zz121210 <65817595+zz121210@users.noreply.github.com>
2020-12-30 16:34:23 +0900

    Add files via upload

--- javascript/fetch.js
+++ javascript/fetch.js
@@ -2,6 +2,9 @@ function fth(file){
     fetch(file).then(function(response){
         response.text().then(function(text){
             document.querySelector('#spot').innerHTML=text;
+            alpa=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+            count = 0;
+            window.scrollTo(0,0);
         })
     })
 }
