17d22f5d78743439c7894c102bc43447d39d2812
zz121210 <65817595+zz121210@users.noreply.github.com>
2020-12-31 13:37:12 +0900

    Add files via upload

---
+++ javascript/history.js
@@ -0,0 +1,4 @@
+const state = { 'page_id': 1, 'user_id': 5 };
+const title = '';
+const url = 'index.html/goldSpoon';
+
---
+++ javascript/job-face.js
@@ -0,0 +1,35 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+  
+      var reader = new FileReader();
+  
+      reader.onload = function(e) {
+        $('.image-upload-wrap').hide();
+  
+        $('.file-upload-image').attr('src', e.target.result);
+        $('.file-upload-content').show();
+  
+        document.querySelector('.image-title').style.display='none';
+      };
+  
+      reader.readAsDataURL(input.files[0]);
+  
+    } else {
+      removeUpload();
+    }
+  }
+  
+  function removeUpload() {
+    document.querySelector('#label-container').innerHTML="";
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+  }
+  $('.image-upload-wrap').bind('dragover', function () {
+          $('.image-upload-wrap').addClass('image-dropping');
+      });
+      $('.image-upload-wrap').bind('dragleave', function () {
+          $('.image-upload-wrap').removeClass('image-dropping');
+  });
+  
+  
